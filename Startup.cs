5e11fbeec8b4e289b31932616a8f1f80afad2214
Zack <zahari.kitanov@abv.bg>
2020-06-01 14:39:52 +0300

    Some additional cleanup.

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Threading.Tasks;
 using BlazorStrap;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MultilevelDropdownMenu.Data;
 
 namespace MultilevelDropdownMenu
 {
@@ -31,7 +23,6 @@ namespace MultilevelDropdownMenu
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBootstrapCss();
-            //services.AddScoped<SelectedMenuItemState>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
